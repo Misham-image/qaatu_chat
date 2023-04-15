ec6e3f5dacfcf4233a0ae11e469b109bfa3fcec6
renjithklm1994 <46755547+renjithklm1994@users.noreply.github.com>
2023-04-15 10:18:32 +0530

    update

--- python/virtual/web/daybook/models.py
+++ python/virtual/web/daybook/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class journels(models.Model):
 
     Products = models.CharField(max_length=100)
-    amount = models.CharField(max_length=100)
-    Invoice = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    Invoice = models.IntegerField()
     Date = models.DateField()
 
 
